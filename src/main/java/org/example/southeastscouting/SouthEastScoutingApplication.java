9feee343a1084a2883635799ae8be61e8298aa0c
haojiecheng <2965908658@qq.com>
2026-03-24 14:15:11 +0800

    initialize project with Spring Boot and Maven wrapper; add .gitignore and .gitattributes

---
+++ src/main/java/org/example/southeastscouting/SouthEastScoutingApplication.java
@@ -0,0 +1,13 @@
+package org.example.southeastscouting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouthEastScoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouthEastScoutingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/southeastscouting/SouthEastScoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.southeastscouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouthEastScoutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
